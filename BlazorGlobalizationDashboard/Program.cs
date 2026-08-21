f01cd0cbd918925d9127a5a9b3d6815b1785f352
Pankaj Kumar <pankaj.v3m@gmail.com>
2026-08-21 18:32:46 +0530

    program file

--- BlazorGlobalizationDashboard/Program.cs
+++ BlazorGlobalizationDashboard/Program.cs
@@ -13,6 +13,12 @@ builder.Services.AddRazorComponents()
 // Add localization with Resources folder
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
+
+
+
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
 // Configure supported cultures
 var supportedCultures = new[]
 {
